600e518580da95477b9d027f6a7a47a74b545cc4
pankokko <teshi41@gmail.com>
2019-11-25 18:38:23 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  root  'test#index'
-    resources :test, only: [:index,:show]
-
+  root  ‘signup#index’
+  resources :signup, only: [:index] do
+    collection do
+      get :login
+      get :registration
+      get :sms_confirmation
+      get :address
+      get :card
+      get :complete
+    end
+  end
 end
