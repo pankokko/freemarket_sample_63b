2848c30ac6f2e5c664a27b0092b0d04da5ef81e3
pankokko <teshi41@gmail.com>
2019-12-03 12:26:17 +0900

    gitignoreに記述追加 createアクションに処理追加

--- app/controllers/exhibit_controller.rb
+++ app/controllers/exhibit_controller.rb
@@ -1,26 +1,28 @@
 class ExhibitController < ApplicationController
   def index
     @exhibit = Exhibit.all.includes(:images)
-    end
+  end
 
-def new
-  @exhibit = Exhibit.new
-  @grandcildren = Category.find(2).children
-  @exhibit.images.new
- end
+  def new
+    @exhibit = Exhibit.new
+    @grandcildren = Category.find(2).children
+    @exhibit.images.new
+   end
 
 
 def create 
    @Exhibit = Exhibit.create(exhibit_params)
    if @Exhibit.save
-    redirect_to root_path    
-end 
+    redirect_to root_path   
+   else 
+    render :new 
+ end 
 end 
 
 
 def exhibit_params
-params.require(:exhibit).permit(:name, :category_id, :buyer_id ,:price, :status, :description, :ship, :ship_fee, :prefecture,:size, images_attributes: [:image]).merge(user_id: current_user.id)
-end
+  params.require(:exhibit).permit(:name, :category_id, :buyer_id ,:price, :status, :description, :ship, :ship_fee, :prefecture,:size, images_attributes: [:image]).merge(user_id: current_user.id)
+ end
 end
 
 
