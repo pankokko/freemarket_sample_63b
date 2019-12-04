14982154c1ec7ba6f913d25f40e367a495634d91
pankokko <teshi41@gmail.com>
2019-12-04 11:48:40 +0900

    ビューの修正

--- app/controllers/exhibit_controller.rb
+++ app/controllers/exhibit_controller.rb
@@ -10,26 +10,28 @@ class ExhibitController < ApplicationController
    end
 
 
-def create 
-   @Exhibit = Exhibit.create(exhibit_params)
-   if @Exhibit.save
-    redirect_to root_path   
-   else 
-    render :new 
- end 
-end 
+  def create 
+    @Exhibit = Exhibit.create(exhibit_params)
+    if @Exhibit.save
+      redirect_to root_path   
+    else 
+      render :new 
+  end 
+  end 
 
-def search 
-@exhibit = Exhibit.search(params[:search])
-end
+  def show 
+    @exhibit = Exhibit.find(params[:id])
+  end
+
+  def search 
+    @exhibit = Exhibit.search(params[:search])
+  end
 
 
-
-
-def exhibit_params
-  params.require(:exhibit).permit(:name, :category_id, :buyer_id ,:price, :status, :description, :ship, :ship_fee, :prefecture,:size, images_attributes: [:image]).merge(user_id: current_user.id)
- end
-end
+  def exhibit_params
+    params.require(:exhibit).permit(:name, :category_id, :buyer_id ,:price, :status, :description, :ship, :ship_fee, :prefecture,:size, images_attributes: [:image]).merge(user_id: current_user.id)
+  end
+  end
 
 
 # exhibitのレコードを作成する
