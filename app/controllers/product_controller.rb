11067f3a86794ca6aa0ee18874bc405984437b46
falex1022 <56875497+falex1022@users.noreply.github.com>
2019-12-05 11:45:05 +0900

    コメント削除

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,9 +2,6 @@ class ProductController < ApplicationController
   def index
   end
   def show
-    # id =  Product.find(params[:id])
-    # @product = id.includes(:images)
-    
     @product = Exhibit.find(params[:id])
     @image = Image.find(params[:id])
     @images = Image.all
@@ -12,10 +9,4 @@ class ProductController < ApplicationController
     @products = Exhibit.where(user_id: @product.user_id).limit(6).order('id DESC')
     binding.pry
   end
-  
-  # private
-  # def product_params
-  #   params.require(:product).permit(:image, :text).merge(user_id: current_user.id)
-  # end
-  
 end
