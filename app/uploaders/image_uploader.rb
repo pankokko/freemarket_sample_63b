a1dac4b3ace10c73ddfc606dcc3f2ada3b9eb24c
herohero1022 <eq646261@gmail.com>
2019-12-09 18:57:15 +0900

    画像修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
 
-  resize_to_fit(100, 100)
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
     storage :file
@@ -18,8 +17,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  process resize_to_limit: [300, 300]
 
 
 
