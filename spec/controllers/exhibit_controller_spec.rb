90e7d6eb33ab62b3f7458883a756996754b19cc3
pankokko <teshi41@gmail.com>
2019-12-10 21:15:25 +0900

    編集画面のビュー崩れを修正

--- spec/controllers/exhibit_controller_spec.rb
+++ spec/controllers/exhibit_controller_spec.rb
@@ -1,31 +1,41 @@
   require 'rails_helper'
 
  RSpec.describe ExhibitController, type: :controller do
-   describe 'GET #new' do
-     before do
-       get :new 
-    end 
-     it "newアクションが実行されるか" do 
-      expect(response).to render_template :new
-    end 
-    it "インスタンスが作成されるか" do 
-      expect(assigns(:exhibit)).to be_a_new(Exhibit)
-  end
-end
-
+   
   describe 'GET #index' do
-    it "indexアクションが動くか" do
-      get :index
-      expect(response).to render_template :index
-    end
-    end
+  before do
+    get :index
+ end 
+  it "newアクションが実行されるか" do 
+   expect(response).to render_template :index
+ end 
+   
 
-    describe 'POST #create' do
-      it "データベースにデータが保存されるか" do
-        expect{
-          post :create, exhibit: attributes_for(:exhibit)
-        }.to change(Exhibit, :count).by(1)
-      end
+#    describe 'GET #new' do
+#      before do
+#        get :new 
+#     end 
+#      it "newアクションが実行されるか" do 
+#       expect(response).to render_template :new
+#     end 
+#     it "インスタンスが作成されるか" do 
+#       expect(assigns(:exhibit)).to be_a_new(Exhibit)
+#   end
+# end
+
+#   describe 'GET #index' do
+#     it "indexアクションが動くか" do
+#       get :index
+#       expect(response).to render_template :index
+#     end
+#     end
+
+#     describe 'POST #create' do
+#       it "データベースにデータが保存されるか" do
+#         expect{
+#           post :create, exhibit: attributes_for(:exhibit)
+#         }.to change(Exhibit, :count).by(1)
+#       end
         
-      end
-  end
+#       end
+#   end
