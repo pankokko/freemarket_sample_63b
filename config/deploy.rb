a5cdf7d277b68c1f20e15f3b4610aaa5fee7162e
pankokko <teshi41@gmail.com>
2019-12-12 11:39:25 +0900

    masterに変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'freemarket_sample_63b'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:herohero1022/freemarket_sample_63b.git'
+set :repo_url,  'git@github.com:pankokko/freemarket_sample_63b.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/altale.pem'] 
+                  keys: ['~/.ssh/my_market.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.177.59.127', user: 'ec2-user', roles: %w{app db web}
+server '3.130.181.145', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
